0dfc8d37609b663c0ee6c34180464be2baa3ed5b
fasalmbt <>
2021-12-08 17:57:05 +0530

    Added files- completed V1.0

--- src/Feditor/NotePad.java
+++ src/Feditor/NotePad.java
@@ -2,8 +2,14 @@ package Feditor;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.awt.Desktop;
 import java.net.URI;
+import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
@@ -11,24 +17,99 @@ import javax.swing.JMenuItem;
 import javax.swing.JOptionPane;
 import javax.swing.JTextArea;
 
+
 public class NotePad {
 	public static void main(String[] args) {
 		JFrame frame = new JFrame("Feditor");
+		frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
 		JTextArea tarea = new JTextArea();
 		JMenuBar menuBar = new JMenuBar();
 		JMenu filemenu = new JMenu("File");
 		JMenu editmenu = new JMenu("Edit");
 		JMenu helpmenu = new JMenu("Help");
-		JMenuItem newitem = new JMenuItem("New");
 		JMenuItem openitem = new JMenuItem("Open File");
+		JFileChooser fopenc = new JFileChooser();
 		JMenuItem saveitem = new JMenuItem("Save");
 		JMenuItem exititem = new JMenuItem("Exit");
 		JMenuItem copyitem = new JMenuItem("Copy");
+		JMenuItem cutitem = new JMenuItem("Cut");
 		JMenuItem pasteitem = new JMenuItem("Paste");
 		JMenuItem selectitem = new JMenuItem("Select All");
 		JMenuItem docsitem = new JMenuItem("Docs");
 		JMenuItem contributeitem = new JMenuItem("Contribute");
 		JMenuItem aboutitem = new JMenuItem("About");
+		openitem.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				int opencheck = fopenc.showOpenDialog(null);
+				if(opencheck == JFileChooser.APPROVE_OPTION) {
+					File fil = new File(fopenc.getSelectedFile().getAbsolutePath());
+					try {
+						String str1 = "", sringlen = "";
+						FileReader freader = new FileReader(fil);
+						BufferedReader breader = new BufferedReader(freader);
+						sringlen = breader.readLine();
+						while((str1 = breader.readLine()) != null) {
+							sringlen = sringlen + '\n' + str1;
+						}
+						tarea.setText(sringlen);
+						breader.close();
+					}
+					catch (Exception evt1) {
+						JOptionPane.showMessageDialog(frame, evt1.getMessage());
+					}
+				}
+			}
+		});
+		saveitem.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				int savecheck = fopenc.showSaveDialog(null);
+				if(savecheck == JFileChooser.APPROVE_OPTION) {
+					File fil = new File(fopenc.getSelectedFile().getAbsolutePath());
+					try {
+	                    FileWriter fwriter = new FileWriter(fil, false);
+	                    BufferedWriter bwriter = new BufferedWriter(fwriter);
+	                    bwriter.write(tarea.getText());
+	                    bwriter.flush();
+	                    bwriter.close();
+	                }
+	                catch (Exception evt) {
+	                    JOptionPane.showMessageDialog(frame, evt.getMessage());
+	                }
+				}
+				
+			}
+		});
+		exititem.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				System.exit(0);			}
+		});
+		copyitem.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				tarea.copy();
+			}
+		});
+		cutitem.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				tarea.cut();
+			}
+		});
+		pasteitem.addActionListener(new ActionListener(){
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				tarea.paste();
+			}
+		});
+		selectitem.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				tarea.selectAll();
+			}
+		});
 		docsitem.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
@@ -69,6 +150,7 @@ public class NotePad {
 				}
 			}
 		});
+		
 		tarea.setEditable(true);
 		frame.add(tarea);
 		frame.setSize(600,600);
@@ -76,11 +158,11 @@ public class NotePad {
 		menuBar.add(filemenu);
 		menuBar.add(editmenu);
 		menuBar.add(helpmenu);
-		filemenu.add(newitem);
 		filemenu.add(openitem);
 		filemenu.add(saveitem);
 		filemenu.add(exititem);
 		editmenu.add(copyitem);
+		editmenu.add(cutitem);
 		editmenu.add(pasteitem);
 		editmenu.add(selectitem);
 		helpmenu.add(docsitem);
@@ -89,3 +171,4 @@ public class NotePad {
 		frame.setJMenuBar(menuBar);
 	}
 }
+
